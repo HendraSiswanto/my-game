8f4ea8c1b51e84bc42cc982368c422d46f0959dc
widyaoctaviana <etik.hendra@gmail.com>
2025-06-14 06:27:32 +0700

    refactor: remove duplicated styles

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -1,15 +1,14 @@
-import { Card, CardBody, Skeleton, SkeletonText } from '@chakra-ui/react'
-
+import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 
 const GameCardSkeleton = () => {
   return (
-  <Card width='200px'>
-        <Skeleton borderRadius={10} overflow='hidden' height='200px'/>
-        <CardBody>
-            <SkeletonText />
-        </CardBody>
-  </Card>
-  )
-}
+    <Card>
+      <Skeleton height="200px" />
+      <CardBody>
+        <SkeletonText />
+      </CardBody>
+    </Card>
+  );
+};
 
-export default GameCardSkeleton
+export default GameCardSkeleton;
