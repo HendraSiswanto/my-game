9c12c6a886119f7c8ecb95dc8ccd6dea0a027da4
widyaoctaviana <etik.hendra@gmail.com>
2025-06-20 06:44:59 +0700

    fixxing all same error of icon

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,26 +1,41 @@
-import { Button, Icon, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import {
+  Button,
+  Icon,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 import usePlatform from "../hooks/usePlatform";
 import { Platform } from "../hooks/UseGames";
 
-interface Props{
-  onSelectPlatform : (platform: Platform) => void
-  selectedPlatform : Platform | null
+interface Props {
+  onSelectPlatform: (platform: Platform) => void;
+  selectedPlatform: Platform | null;
 }
 
-const PlatformSelector = ({onSelectPlatform, selectedPlatform} :Props) => {
-  const { data,error } = usePlatform();
+const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
+  const { data, error } = usePlatform();
 
-  if (error) return null  
+  if (error) return null;
 
   return (
     <Menu>
-      <MenuButton as={Button} rightIcon={<Icon as= {BsChevronDown} />}>
-      { selectedPlatform?.name || 'Platforms'}
+      <MenuButton
+        as={Button}
+        rightIcon={<Icon as={BsChevronDown as React.ElementType} />}
+      >
+        {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
         {data.map((platform) => (
-          <MenuItem onClick={() => onSelectPlatform(platform)} key={platform.id}>{platform.name}</MenuItem>
+          <MenuItem
+            onClick={() => onSelectPlatform(platform)}
+            key={platform.id}
+          >
+            {platform.name}
+          </MenuItem>
         ))}
       </MenuList>
     </Menu>
