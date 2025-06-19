1b3494ad2e07d1026451e27ac195829f849bd1c9
widyaoctaviana <etik.hendra@gmail.com>
2025-06-20 06:20:22 +0700

    error of iconMap undifined reactElement

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -7,9 +7,9 @@ import {
   FaAndroid,
   FaPlaceOfWorship,
 } from "react-icons/fa";
-import {MdPhoneIphone} from 'react-icons/md'
-import {SiNintendo} from 'react-icons/si'
-import {BsGlobe} from 'react-icons/bs'
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
 import { HStack, Icon, Text } from "@chakra-ui/react";
 import { Platform } from "../hooks/UseGames";
 import { IconType } from "react-icons";
@@ -19,8 +19,7 @@ interface Props {
 }
 
 const PlatformIconList = ({ platforms }: Props) => {
-
-  const iconMap:{[key: string]: IconType} ={
+  const iconMap: { [key: string]: IconType } = {
     pc: FaWindows,
     playstation: FaPlaystation,
     xbox: FaXbox,
@@ -29,13 +28,17 @@ const PlatformIconList = ({ platforms }: Props) => {
     linux: FaLinux,
     android: FaAndroid,
     ios: MdPhoneIphone,
-    web: BsGlobe
-  }
-    
+    web: BsGlobe,
+  };
+
   return (
     <HStack marginY={1}>
       {platforms.map((platform) => (
-        <Icon key={platform.id} as={iconMap[platform.slug]} color='gray.500'/>
+        <Icon
+          key={platform.id}
+          as={iconMap[platform.slug] as React.ElementType}
+          color="gray.500"
+        />
       ))}
     </HStack>
   );
